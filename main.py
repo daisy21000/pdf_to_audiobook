a33ab0a8b7753e936889ef3790f55df5ab8276d7
daisy21000 <maryam.shafiq219@yahoo.com>
2023-09-06 15:32:43 +0100

    Change Filenames to placeholders

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from pypdf import PdfReader
 from gtts import gTTS
 
-reader = PdfReader('Drift.pdf')
+reader = PdfReader('Book.pdf')
 text = ''
 for page in reader.pages:
     text += page.extract_text() + '\n'
 
 tts = gTTS(text, lang='en')
 
-tts.save('drift.mp3')
+tts.save('audiobook.mp3')
